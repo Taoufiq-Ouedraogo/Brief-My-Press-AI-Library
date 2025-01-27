9e5746fd5c155c1546f178b283b0fa4c6b39669d
Taoufiq Ouedraogo <taoufiq.ouedraogo@outlook.com>
2025-01-27 03:43:53 +0100

    commit package git v9

--- bmp_lib/__init__.py
+++ bmp_lib/__init__.py
@@ -1,15 +1,6 @@
-from .bmp_func import bmp_summaries_and_audio
+from .bmp_func import *
 
 
 
 # Build the package
 # python setup.py sdist bdist_wheel
-
-# Install package locally
-# pip install -e .
-
-# git tag 0.0.1 -m "New Release"
-# git push origin 0.0.1
-
-
- 
